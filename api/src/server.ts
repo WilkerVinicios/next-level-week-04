10a6a10a66e3271f453fc61fd9e1f5d338f9dcc7
WilkerVinicios <44215818+WilkerVinicios@users.noreply.github.com>
2021-02-23 21:56:36 -0300

    segundo dia;

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,11 @@
-import express, { response } from 'express';
+import 'reflect-metadata'
+import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-//http://localhost:3030/users
-app.get("/", (request, response) => {
-    //return response.send("Hello World - NLW04");
-    return response.json({message: "Hello World - NLW04"});
-});
-
-app.post("/", (request, response) => {
-    return response.json({message: "Os dados foram salvos com sucesso!"})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3030, () => console.log("Server is running!"));
