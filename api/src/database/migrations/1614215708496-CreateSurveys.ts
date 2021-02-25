f3adf4022b3c8c7d23b0dbca9d708cdc7f99c614
WilkerVinicios <44215818+WilkerVinicios@users.noreply.github.com>
2021-02-25 14:39:58 -0300

    terceiro dia;

--- api/src/database/migrations/1614215708496-CreateSurveys.ts
+++ api/src/database/migrations/1614215708496-CreateSurveys.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUsers1614122809577 implements MigrationInterface {
+export class CreateSurveys1614215708496 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "users",
+                name: "surveys",
                 columns: [
                     {
                         name: "id",
@@ -13,11 +13,11 @@ export class CreateUsers1614122809577 implements MigrationInterface {
                         isPrimary: true
                     },
                     {
-                        name: "name",
+                        name: "title",
                         type: "varchar"
                     },
                     {
-                        name: "email",
+                        name: "description",
                         type: "varchar"
                     },
                     {
@@ -31,7 +31,7 @@ export class CreateUsers1614122809577 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("users");
+        await queryRunner.dropTable("surveys");
     }
 
 }
